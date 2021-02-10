f7f26912310466d7e0458e0b4817f2735c7c65a3
kurniawan26 <kirigaya.kurniawan@gmail.com>
2021-02-10 20:42:52 +0700

    Komponen Header Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 import "assets/scss/style.scss";
+import landingPage from 'pages/landingPage';
 
 function App() {
   return (
     <div className="App">
-      hai dunia
+      <Router>
+        <Route path="/" component={landingPage}></Route>
+      </Router>
     </div>
   );
 }
