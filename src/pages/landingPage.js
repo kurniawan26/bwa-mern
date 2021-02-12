6582c6fa5d9dd4aa79a94fd115bae409b35e82d9
kurniawan26 <kirigaya.kurniawan@gmail.com>
2021-02-12 23:07:57 +0700

    parts of category finished

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Header from "parts/Header"
-import Hero from 'parts/Hero'
-import MostPicked from "parts/mostPicked"
+import Header from "parts/Header";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 
-import LandingPage from "json/landingPage.json"
+import LandingPage from "json/landingPage.json";
+import Categories from "parts/Categories";
 
 export default class landingPage extends Component {
-    render() {
-        return (
-            <>
-                <Header {...this.props}></Header>
-                <Hero data={LandingPage.hero}/>
-                <MostPicked data={LandingPage.mostPicked}/>
-            </>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+  render() {
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={LandingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={LandingPage.mostPicked}
+        />
+        <Categories data={LandingPage.categories}/>
+      </>
+    );
+  }
 }
--- src/parts/mostPicked.js
+++ src/parts/mostPicked.js
@@ -3,12 +3,15 @@ import Button from "elements/button";
 
 export default function mostPicked(props) {
   return (
-    <section className="container">
+    <section className="container" ref={props.refMostPicked}>
       <h4 className="mb-3">Most Picked</h4>
       <div className="container-grid">
         {props.data.map((item, index) => {
           return (
-            <div className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}>
+            <div
+              key={`mostpicked-${index}`}
+              className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}
+            >
               <div className="card card-featured">
                 <div className="tag">
                   ${item.price}
@@ -30,7 +33,7 @@ export default function mostPicked(props) {
                     <h5>{item.name}</h5>
                   </Button>
                   <span>
-                      {item.city}, {item.country}
+                    {item.city}, {item.country}
                   </span>
                 </div>
               </div>
