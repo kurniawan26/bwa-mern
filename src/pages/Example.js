ad90ec298b5f52c1ef1f71bf8aa1f611bec2b39c
kurniawan26 <kirigaya.kurniawan@gmail.com>
2021-09-22 23:27:20 +0700

    make breadcrumb and inputDate Components

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
-
-import InputNumber from "elements/Form/InputNumber";
+import Breadcrumb from "elements/Breadcrumb";
 
 export default class Example extends Component {
-  state = {
-    value: "",
-  };
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
   render() {
+    const breadcrumbList = [
+      { pageTitle: "Home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
+
     return (
       <div className="container">
         <div
@@ -18,14 +15,7 @@ export default class Example extends Component {
           style={{ height: "100vh" }}
         >
           <div className="col-auto">
-            <InputNumber
-              max={30}
-              suffix=" night"
-              isSuffixPlural
-              onChange={this.handleChange}
-              name="value"
-              value={this.state.value}
-            />
+            <Breadcrumb data={breadcrumbList} />
           </div>
         </div>
       </div>
